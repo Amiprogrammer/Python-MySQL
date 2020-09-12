9f89fbafb8a56252622fac66f3e62ce5d714dfcb
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-12 16:06:12 +0900

    Learn how to drop a table in database Python MySQL

--- 09_Drop Table/main.py
+++ 09_Drop Table/main.py
@@ -1,1 +1,15 @@
-main.py
+import mysql.connector
+
+db = mysql.connector.connect(
+	host="localhost",
+	username="root",
+	password="jmq123jj",
+	database="mydatabase"
+)
+
+mycursor = db.cursor()
+
+# sql = "DROP TABLE costumer"
+sql = "DROP TABLE IF EXISTS costumer"
+
+mycursor.execute(sql)
