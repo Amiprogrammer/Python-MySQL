603e17f68fe4a7535d603364c8635955b0925c6d
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-12 22:23:19 +0900

    Learn update record Python MySQL

---
+++ 10_Update Record/main.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+	host="localhost",
+	username="root",
+	password="jmq123jj",
+	database="mydatabase"
+)
+
+mycursor = db.cursor()
+
+# mycursor.execute("CREATE TABLE costumer (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+# mycursor.execute("SHOW TABLES")
+
+# for x in mycursor:
+	# print(x)
+
+# sql = "INSERT INTO costumer (name,address) VALUES (%s,%s)"
+# val = [
+# 	("Rivaldo Li","Becora"),
+# 	("Luduvina Castro","Lahane"),
+# 	("Lurdes Baquita","Audian"),
+# 	("Genilda Li","Perunas"),
+# 	("Joaquim Gomes","Vilaverde")
+# ]
+
+# mycursor.executemany(sql,val)
+
+# db.commit()
+
+# print(mycursor.rowcount, "Inserted record(s).")
+
+mycursor.execute("UPDATE costumer SET address = \"Tasi3\" WHERE name = \"Genilda Li\"")
+
+db.commit()
+
+print(mycursor.rowcount, " record(s) affected.")
