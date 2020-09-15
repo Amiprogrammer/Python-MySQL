20c4348dfa25bb468d687a95bcca020ea07a1705
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-09-15 14:31:46 +0900

    Learn Limit record Python MySQL

--- 11_Limit/main.py
+++ 11_Limit/main.py
@@ -1,1 +1,17 @@
-main.py
+import mysql.connector
+
+db = mysql.connector.connect(
+	host="localhost",
+	username="root",
+	password="jmq123jj",
+	database="mydatabase"
+)
+
+mycursor = db.cursor()
+
+mycursor.execute("SELECT * FROM costumer LIMIT 2")
+
+result = mycursor.fetchall()
+
+for x in result:
+	print(x)
