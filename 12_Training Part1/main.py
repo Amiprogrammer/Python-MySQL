744d384c0df7364b9422347d5cb9826f4b00d978
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-09 10:47:44 +0900

    how to make associate python languague with mysql database server

--- 12_Training Part1/main.py
+++ 12_Training Part1/main.py
@@ -10,20 +10,4 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE TABLE estudent (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(250), gender CHAR(1), address VARCHAR(250))")
-
-sql = "INSERT INTO estudent (name,gender,address) VALUES (%s,%s,%s)"
-val = [
-    ("joao nunes","m","becora"),
-    ("sebastiao soares","m","balide"),
-    ("mariana tavares","f","matadoru"),
-    ("celestino alves","m","quintal-kiik"),
-    ("helena martins","f","tibar")
-]
-
-mycursor.executemany(sql,val)
-
-if( mycursor.rowcount > 0 ):
-    print(mycursor.rowcount, "inserted record(s)!")
-else:
-    print("insert failled!")
+print("=== Databases Application ===")
