ea8534e21ecc983a35e55d63cec025e72d7c224c
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-09 10:45:24 +0900

    Create table and insert a date in mysql_training database

--- 12_Training Part1/main.py
+++ 12_Training Part1/main.py
@@ -5,13 +5,25 @@ mydb = mysql.connector.connect(
     host="localhost",
     username="root",
     password="root",
+    database="mysql_training"
 )
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE mysql_traning") # create my database
+# mycursor.execute("CREATE TABLE estudent (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(250), gender CHAR(1), address VARCHAR(250))")
 
-mycursor.execute("SHOW DATABASES")
+sql = "INSERT INTO estudent (name,gender,address) VALUES (%s,%s,%s)"
+val = [
+    ("joao nunes","m","becora"),
+    ("sebastiao soares","m","balide"),
+    ("mariana tavares","f","matadoru"),
+    ("celestino alves","m","quintal-kiik"),
+    ("helena martins","f","tibar")
+]
 
-for x in mycursor:
-    print(x)
+mycursor.executemany(sql,val)
+
+if( mycursor.rowcount > 0 ):
+    print(mycursor.rowcount, "inserted record(s)!")
+else:
+    print("insert failled!")
