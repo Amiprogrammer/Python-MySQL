540debcac9f1f848ad52c6d50a7bae7d798122f8
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-27 21:08:50 +0900

    Insert records and check the ID in last

--- 04_Inset Into Table/main.py
+++ 04_Inset Into Table/main.py
@@ -10,18 +10,19 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 sql = "INSERT INTO estudent (name,email,address) VALUES (%s,%s,%s)"
+val = ("eleandro silva","eleandrosilva@gmail.com","likisa")
 # val = ("juliao martins","juliaomartins@gmail.com","tasi-3")
 # more inserted
-val = [
-    ("carmo da silva","carmodasilva@gmail.com","bebonuk"),
-    ("joana carvalho","joanacarvalho@gmail.com","lahane"),
-    ("celestina maria","celestinamaria@gmail.com","kuluhun"),
-    ("jaime perdeus","jaimeperdeus@gmail.com","tibar")
-]
+# val = [
+#     ("carmo da silva","carmodasilva@gmail.com","bebonuk"),
+#     ("joana carvalho","joanacarvalho@gmail.com","lahane"),
+#     ("celestina maria","celestinamaria@gmail.com","kuluhun"),
+#     ("jaime perdeus","jaimeperdeus@gmail.com","tibar")
+# ]
 
 
-mycursor.executemany(sql,val)
+mycursor.execute(sql,val)
 
 mydb.commit()
 
-print(mycursor.rowcount, "record(s) inserted!")
+print(mycursor.rowcount, "record(s) inserted! ID :", mycursor.lastrowid)
