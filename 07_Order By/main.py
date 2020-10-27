1557a0a2b4450c7a80b39a33eca3007e5a5dbfd9
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-27 21:38:06 +0900

    set a ascending to view record(s)

--- 07_Order By/main.py
+++ 07_Order By/main.py
@@ -8,3 +8,10 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM estudent ORDER BY name ASC")
+
+result = mycursor.fetchall()
+
+for x in result:
+    print(x)
