a36a8478eafd29cfa0c0ffa4acfebcdcf0c53c05
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-27 22:12:01 +0900

    Create a new database and table to try drop database and table

--- 09_Drop Database or Table/main.py
+++ 09_Drop Database or Table/main.py
@@ -4,7 +4,10 @@ mydb = mysql.connector.connect(
             host="localhost",
             username="root",
             password="root",
-            database="testing"
+            database="testing2"
 )
 
 mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE testing2")
+mycursor.execute("CREATE TABLE costumer(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
