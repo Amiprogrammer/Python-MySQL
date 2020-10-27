41e8e2810273eb5d726b0aca9b16f10d70dad035
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-27 20:33:31 +0900

    check my database if exists

--- 02_Create Database/main.py
+++ 02_Create Database/main.py
@@ -8,4 +8,9 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-mycursor.execute("CREATE DATABASE testing")
+# mycursor.execute("CREATE DATABASE testing")
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
