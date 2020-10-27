1730b98abec7fc525afa961ff91b954250c6d1d3
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-27 20:50:46 +0900

    check again my table in database

--- 03_Create Table/main.py
+++ 03_Create Table/main.py
@@ -11,9 +11,9 @@ mycursor = mydb.cursor()
 
 # mycursor.execute("CREATE TABLE estudent (name VARCHAR(255), email VARCHAR(255), address VARCHAR(255))")
 
-# mycursor.execute("SHOW TABLES")
+mycursor.execute("SHOW TABLES")
 
-# for x in mycursor:
-#    print(x)
+for x in mycursor:
+   print(x)
 
-mycursor.execute("ALTER TABLE estudent ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+# mycursor.execute("ALTER TABLE estudent ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
