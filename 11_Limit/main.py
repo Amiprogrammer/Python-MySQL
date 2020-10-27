967b5cfe4efdd6f9064ebb7fd77f49e43c008776
JM-Kodigu <62004484+jm-kodigu@users.noreply.github.com>
2020-10-27 22:26:46 +0900

    Inserted 5 record(s) to the table

--- 11_Limit/main.py
+++ 11_Limit/main.py
@@ -8,3 +8,18 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+
+sql = "INSERT INTO estudent (name,email,address) VALUES (%s,%s,%s)"
+val = [
+    ("maia soares","maiasoares@gmail.com","tasi-3"),
+    ("hernanda silva","hernasilva@gmail.com","becora"),
+    ("silvia sousa","silviasou@gmail.com","bidau"),
+    ("jaime de jesus","jaimedjesus@gmail.com","beto"),
+    ("cinto cabral","cintocabral@gmail.com","lahane")
+]
+
+mycursor.executemany(sql,val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) inserted!")
